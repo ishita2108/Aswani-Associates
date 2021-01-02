ac222ba7636d87c51b6b15fe515735cd3cf81123
ishita2108 <iwinner21@gmail.com>
2021-01-02 17:59:16 +0530

    Contact

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+
+const contactMobile = document.getElementById("contact-mobile")
+const contactDesktop = document.getElementById("contact")
+
+if(window.matchMedia("(max-width: 768px)").matches){
+    contactDesktop.classList.add("none")
+    contactMobile.classList.remove("none")
+}
+else{
+    contactDesktop.classList.remove("none")
+    contactMobile.classList.add("none")
+}
+
+
+
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
@@ -51,17 +66,35 @@ ulItems.addEventListener('mouseleave', function(){
 const myFunction = (e) => {
     console.log(window.scrollY);
     const yoffset = window.scrollY;
-    if(yoffset<456){
+    if(yoffset<460){
         navService.classList.remove("active")
+        navFeatures.classList.remove("active")
+        navContact.classList.remove("active")
         navHome.classList.add("active")
     }
-    else if(yoffset>456){
+    else if(yoffset>460 && yoffset<850){
         navService.classList.add("active")
         navHome.classList.remove("active")
-
+        navFeatures.classList.remove("active")
+        navContact.classList.remove("active")
+    }
+    else if(yoffset>950 && yoffset<1214){
+        navFeatures.classList.add("active")
+        navService.classList.remove("active")
+        navHome.classList.remove("active")
+        navContact.classList.remove("active")
+    }
+    else if(yoffset>1214){
+       navContact.classList.add("active")
+        navFeatures.classList.remove("active")
+        navService.classList.remove("active")
+        navHome.classList.remove("active")
     }
 
 }
 
 const navHome = document.getElementById("nav-home")
 const navService = document.getElementById("nav-services")
+const navFeatures = document.getElementById("nav-features")
+const navContact = document.getElementById("nav-contact")
+
